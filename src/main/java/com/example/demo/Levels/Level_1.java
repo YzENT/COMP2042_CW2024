d85ead831c611ed9723b63eff35a91856b40d49c
YzENT <153203252+YzENT@users.noreply.github.com>
2024-11-04 15:33:16 +0800

    Correction for lose condition in Level_1.java

--- src/main/java/com/example/demo/Levels/Level_1.java
+++ src/main/java/com/example/demo/Levels/Level_1.java
@@ -19,7 +19,7 @@ public class Level_1 extends LevelParent {
 	@Override
 	protected void checkIfGameOver() {
 		if (userIsDestroyed()) {
-			gameStatus(GameStatus.VICTORY);
+			gameStatus(GameStatus.DEFEAT);
 		}
 		else if (userHasReachedKillTarget()) {
 			goToNextLevel(NEXT_LEVEL);
